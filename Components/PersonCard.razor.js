a7df9a192901d59016c20656dc18971c547080a2
Tadej Mašić <masictadej@gmail.com>
2024-09-21 21:18:12 +0200

    per component JS files

--- Components/PersonCard.razor.js
+++ Components/PersonCard.razor.js
@@ -1,22 +1,28 @@
-﻿export function mouseMoveOnCard(event, id) {
-    event.preventDefault;
+﻿export function StartMovingPersonCard(mouseArgs, personCardId) {
 
-    console.log("mouseMoveOnCard: here#######");
+    //console.log("mouseMoveOnCard: here#######");
+    //console.log(mouseArgs);
 
-    let card = document.getElementById(id);
+    var personCard = document.getElementById(personCardId);
+    console.log("PersonCard:", personCard);
+    personCard.addEventListener("onmouseup", sendNewPersonCardCoordinates(mouseArgs))
 
-    let x = event.movementX + card.offsetLeft;
-    let y = event.movementY + card.offsetTop;
-    let styles = `position: abosulute;
-                  font-size: 1rem;
-                  width: 5rem;
-                  top:${y}px; left:${x}px;`;
+    //let card = document.getElementById(id);
 
-    card.style = styles;
+    //let x = event.movementX + card.offsetLeft;
+    //let y = event.movementY + card.offsetTop;
+    //let styles = `position: abosulute;
+    //              font-size: 1rem;
+    //              width: 5rem;
+    //              top:${y}px; left:${x}px;`;
+
+    //card.style = styles;
 
 
 }
 
-window.TrackCoordinates = (e) => {
-    console.log(e);
+ 
+ function sendNewPersonCardCoordinates(mouseArgs) {
+     console.log("sendNewPersonCardCoordinates", mouseArgs);
+
 }
