52cfed3ebe4142135e4c8befc49915521c15f034
Tadej Mašić <masictadej@gmail.com>
2024-09-21 08:05:24 +0200

    endofwork

---
+++ Components/PersonCard.razor.js
@@ -0,0 +1,22 @@
+﻿export function mouseMoveOnCard(event, id) {
+    event.preventDefault;
+
+    console.log("mouseMoveOnCard: here#######");
+
+    let card = document.getElementById(id);
+
+    let x = event.movementX + card.offsetLeft;
+    let y = event.movementY + card.offsetTop;
+    let styles = `position: abosulute;
+                  font-size: 1rem;
+                  width: 5rem;
+                  top:${y}px; left:${x}px;`;
+
+    card.style = styles;
+
+
+}
+
+window.TrackCoordinates = (e) => {
+    console.log(e);
+}
