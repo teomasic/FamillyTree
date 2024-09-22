c16a7b71c58cc979039c997996e6fd6503a5dbcd
Tadej Mašić <masictadej@gmail.com>
2024-09-22 08:20:08 +0200

    endofwork

--- Components/PersonCard.razor.js
+++ Components/PersonCard.razor.js
@@ -1,28 +1,62 @@
-﻿export function StartMovingPersonCard(mouseArgs, personCardId) {
+﻿var personCard;
 
-    //console.log("mouseMoveOnCard: here#######");
-    //console.log(mouseArgs);
+export async function Init(dotnetRef) {
+    personCard = new PersonCard(dotnetRef);
+}
 
-    var personCard = document.getElementById(personCardId);
-    console.log("PersonCard:", personCard);
-    personCard.addEventListener("onmouseup", sendNewPersonCardCoordinates(mouseArgs))
 
-    //let card = document.getElementById(id);
+export function StartMovingPersonCard(mouseArgs, personCardId) {
+    personCard.StartMovingPersonCard(mouseArgs, personCardId);
+}
 
-    //let x = event.movementX + card.offsetLeft;
-    //let y = event.movementY + card.offsetTop;
-    //let styles = `position: abosulute;
-    //              font-size: 1rem;
-    //              width: 5rem;
-    //              top:${y}px; left:${x}px;`;
 
-    //card.style = styles;
 
+
+class PersonCard {
+
+    dotnetRef;
+    personCard;
+
+    constructor(dotnetRef) {
+        this.dotnetRef = dotnetRef;
+    }
+
+
+
+    StartMovingPersonCard(mouseArgs, personCardId) {
+
+        personCard = document.getElementById(personCardId);
+        //personCard.addEventListener("onmousemove", movePersonCard(mouseArgs, personCard));
+        personCard.addEventListener("onmouseup", this.sendNewPersonCardCoordinates(mouseArgs));
+
+
+
+    }
+
+    movePersonCard = (mouseArgs, personCard) => {
+        console.log("movePersonCard");
+        let x = mouseArgs.movementX + personCard.offsetLeft;
+        let y = mouseArgs.movementY + personCard.offsetTop;
+        let styles = `position: abosulute;
+                      font-size: 1rem;
+                      width: 5rem;
+                      top:${y}px; left:${x}px;`;
+
+        personCard.style = styles;
+    }
+
+
+
+    // CALL DOTNET METHODS WITH JS
+    sendNewPersonCardCoordinates = (mouseArgs) => {
+        console.log("sendNewPersonCardCoordinates", mouseArgs);
+        this.dotnetRef.invokeMethodAsync("SavePesrsonCardAfterMoving", mouseArgs);
+        //personCard.removeEventListener("onmouseup", this.sendNewPersonCardCoordinates);
+    }
 
 }
 
- 
- function sendNewPersonCardCoordinates(mouseArgs) {
-     console.log("sendNewPersonCardCoordinates", mouseArgs);
 
-}
+
+
+
