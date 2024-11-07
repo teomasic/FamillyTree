8b0be4a5014a6a581d1d1c32482d814562b3703a
Tadej Mašić <masictadej@gmail.com>
2024-11-07 22:12:55 +0100

    add Typesript to project

---
+++ scripts/Home.razor.ts
@@ -0,0 +1,46 @@
+﻿
+var home;
+
+export async function Init() {
+    home = new Home();
+}
+
+
+class Home {
+
+    tree_container: HTMLDivElement;
+    tree_content: HTMLDivElement;
+
+    constructor() {
+        this.registerMouseEvent();
+    }
+
+    tree_content_scale = 1;
+
+    registerMouseEvent = () => {
+        this.tree_container = document.querySelector(".tree-container") as HTMLDivElement;
+        this.tree_content = document.querySelector(".tree-content") as HTMLDivElement;
+
+
+        this.tree_container.addEventListener('wheel', (e) => {
+            console.log("here");
+            this.tree_container_wheel(e);
+        });
+
+
+
+    }
+
+    tree_container_wheel = (e) => {
+        e.preventDefault();
+        const zoomFactor = 0.1;
+        if (e.deltaY < 0) {
+            // Zoom in
+            this.tree_content_scale += zoomFactor;
+        } else {
+            // Zoom out
+            this.tree_content_scale = Math.max(0.5, this.tree_content_scale - zoomFactor); // Do not allow to zoom out too much
+        }
+        this.tree_content.style.transform = `scale(${this.tree_content_scale})`;
+    }
+}
